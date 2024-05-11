0bab5ebbd914336fb5d98b624e5fbb7b2ed11930
MD YUNUS <115855149+yunus25jmi1@users.noreply.github.com>
2024-05-11 12:25:26 +0530

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=yunus-${PORT} --advertise-exit-node
+/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=heroku-app
 echo Tailscale started
-ALL_PROXY=socks5://localhost:1055/
-gunicorn --bind 0.0.0.0:${PORT} --workers 2 --threads 2 app.wsgi:app
+ALL_PROXY=socks5://localhost:1055/ /app/my-app
